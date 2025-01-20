50da4d077f2b3f0df144a86393046cd7b11068e8
abdullahcodeacademy <abdullahcodeacademy@gmail.com>
2025-01-20 02:49:16 +0000

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   safelist: [
-    'text-green-700',
-    'text-red-700',
-    'text-blue-700',
+    'bg-blue-500',
+    'hover:bg-blue-700',
+    'bg-yellow-500',
+    'hover:bg-yellow-700',
+    'bg-red-500',
+    'hover:bg-red-700',
+    'disabled:bg-blue-300',
+    'disabled:bg-yellow-300',
+    'disabled:bg-red-300',
   ],
   plugins: [],
 }
